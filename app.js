31a827869a7e9c4f1830f469d9b984dec51791fc
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-17 22:21:59 +0530

    update: elements selected in js

---
+++ app.js
@@ -0,0 +1,4 @@
+const birthDay = document.querySelector(".input-birthday");
+const btnPalindromeCheck = document.querySelector("#btn-checkPalindrome");
+const message = document.querySelector("#message");
+
