814b5887d840844778ca71959f2b057cc2db3b0b
Sunil Vishwakarma <71932768+linkinsunil@users.noreply.github.com>
2021-08-17 23:23:50 +0530

    wip some testing

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ const birthDay = document.querySelector(".input-birthday");
 const btnPalindromeCheck = document.querySelector("#btn-checkPalindrome");
 const message = document.querySelector("#message");
 
+btnPalindromeCheck.addEventListener("click", checkPalindrome)
+
+function checkPalindrome(){
+    console.log(birthDay.value)
+    console.log(reverseFunc(birthDay.value));
+    // message.innerText = reverseFunc(birthDay);
+}
+
+function reverseFunc(str){
+    const reversedStr = str.split("-").reverse().join("");
+    return reversedStr;
+}
